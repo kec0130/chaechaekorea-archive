de86b3419b56a12634890158e48396bbc60a91a3
eunchae <kec0130@gmail.com>
2023-12-01 23:45:47 +0900

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
-import { Post, allPosts } from 'contentlayer/generated';
+import Image from 'next/image';
+import { allPosts } from 'contentlayer/generated';
 import PostList from '@/components/Blog/PostList';
 
 export default function Home() {
   const posts = allPosts.sort((a, b) => (b.date > a.date ? 1 : -1));
-  const duplicatedPosts: Post[] = Array(10).fill(posts).flat();
 
   return (
     <>
-      <section className="h-[50dvh] bg-gradient-to-r from-cyan-500 to-blue-500 py-40"></section>
+      <section className="relative flex h-[50dvh] items-center justify-center px-4">
+        <div className="absolute inset-0 -z-10 overflow-hidden">
+          <Image
+            src="/images/main-cover-01.jpg"
+            alt="Welcome to ChaeChae Korea Homepage"
+            width={1920}
+            height={1280}
+            sizes="100vw"
+            className="h-full w-full object-cover"
+            priority
+          />
+        </div>
+        <div className="bg-black/60 px-4 py-8 text-center text-white lg:px-8 lg:py-10">
+          <h1 className="mb-6 text-3xl font-semibold md:text-4xl lg:text-5xl">
+            Discover the Authentic Korea
+          </h1>
+          <p className="text-md md:text-lg lg:text-xl">
+            Explore Korea through the eyes of locals!
+            <br />
+            Get authentic insights on travel, cuisine, daily life, and the vibrant culture of Korea.
+          </p>
+        </div>
+      </section>
       <main className="container-xl pt-16">
         <section>
-          <PostList posts={duplicatedPosts} />
+          <PostList posts={posts} />
         </section>
       </main>
     </>
--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const isHome = pathname === '/';
 
   return (
-    <header className={clsx({ 'absolute left-0 top-0 w-full': isHome })}>
+    <header className={clsx({ 'absolute left-0 top-0 z-10 w-full': isHome })}>
       <div className={clsx('container-xl py-3 md:py-5', { 'text-white': isHome })}>
         <Link href="/" className="text-xl font-bold md:text-2xl">
           ChaeChae Korea
