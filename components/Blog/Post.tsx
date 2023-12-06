5a5ba8083d577e4707b209653e33738099a0dee7
eunchae <kec0130@gmail.com>
2023-12-07 00:06:04 +0900

    Fix article header style

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -1,31 +1,29 @@
 import dayjs from 'dayjs';
 import Link from 'next/link';
 import { Post } from 'contentlayer/generated';
-
 import Mdx from '../Mdx';
 
 const Post = ({ post }: { post: Post }) => {
   return (
     <article className="prose mx-auto py-10 lg:prose-lg prose-figcaption:text-sm">
       <header>
-        <div className="not-prose mb-4">
+        <div className="mb-4">
           <Link
             href={`/category/${post.category}`}
-            className="font-semibold uppercase text-gray-500"
+            className="font-semibold uppercase text-gray-500 no-underline"
           >
             {post.category}
           </Link>
         </div>
-        <h1>{post.title}</h1>
-        <div className="not-prose text-sm text-gray-500 lg:text-base">
+        <h1 className="!mb-6">{post.title}</h1>
+        <div className="text-sm text-gray-500 lg:text-base">
           <time dateTime={dayjs(post.date).format('YYYY-MM-DD')}>
             {dayjs(post.date).format('MMM D, YYYY')}
           </time>
-          <span className="px-2">·</span>
-          {post.readTime} min read
+          <span className="before:px-[6px] before:content-['·']">{post.readTime} min read</span>
         </div>
       </header>
-      <hr />
+      <hr className="not-prose my-8 lg:my-10" />
       <div>
         <Mdx code={post.body.code} />
       </div>
